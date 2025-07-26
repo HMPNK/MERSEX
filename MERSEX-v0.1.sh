c9dae100f3836339cb8d181b4515721c77df8659
HMPNK <51913753+HMPNK@users.noreply.github.com>
2025-07-26 11:28:13 +0200

    Update MERSEX-v0.1.sh

--- MERSEX-v0.1.sh
+++ MERSEX-v0.1.sh
@@ -49,7 +49,7 @@ gunzip -c all.kmers.gz > start &
 
 #final table merge, Kmer-positive female/male counting and compress
 export FCOUNT="$(awk '{if($1!="INPUT:" && $1!="OUTPUT:"){print};if($1=="OUTPUT:"){exit;}}' union-op-def.file | grep ^f | wc -l)"
-awk '{if($1!="INPUT:" && $1!="OUTPUT:"){print};if($1=="OUTPUT:"){exit;}}' union-op-def.file | awk 'BEGIN{text="start";} {text=text" "$1;} END{print "paste "text; }' | bash | parallel --keep-order -j 16 -l 1000000 --pipe 'mawk -v cmin=2 -v fcount=$FCOUNT -f count.samples.awk - ' | pigz -c > FINAL-table.tsv.gz
+awk '{if($1!="INPUT:" && $1!="OUTPUT:"){print};if($1=="OUTPUT:"){exit;}}' union-op-def.file | awk 'BEGIN{text="start";} {text=text" "$1;} END{print "paste "text; }' | bash | parallel --keep-order -j 16 -l 1000000 --pipe 'mawk -v cmin1=2 -v cmin2=1 -v fcount=$FCOUNT -f count.samples.awk - ' | pigz -c > FINAL-table.tsv.gz
 
 #clean-up
 #remove FIFOs

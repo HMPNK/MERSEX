3276dc84c23a8f6b986f396f51e0b3a0724374ba
HMPNK <51913753+HMPNK@users.noreply.github.com>
2025-07-28 12:44:39 +0200

    a bit more comfort
    Added variables for CPU and job concurrency
    Added low seq. coverage option

---
+++ MERSEX-v0.2.sh
@@ -0,0 +1,84 @@
+#!/usr/bin/env bash
+set -e
+set -o pipefail
+set -u
+
+##A PIPELINE FOR FINDING SEX-specific kmers ##
+#NOT for POOLSEX data! Needs wgs sequencing from individuals!
+#needs at least 6-10x whole genome coverage per sample
+#Set LOWCOV=1 for datasets with less than 6x whole genome coverage
+#read files MUST be sorted by sex and go to folder "males" and folder "females"
+#read files naming scheme MUST BE <sample-id>_1.fq.gz and <sample-id>_2.fq.gz !!!
+
+#settings (currently optimized for server with >=256 GB RAM, >=1TB FREE DISK, >=96 CPU THREADS)
+KMCTHREADS=20 #kmc kmer counting of fastq files, number of threads
+KMCJOBS=4     #kmc kmer counting of fastq files, number of kmc jobs running in parallel
+COUNTJOBS=8   #jobs running in parallel, if counting kmer frequencies over all detected kmers in all samples
+MERGEJOBS=16  #jobs running in parallel, if merging all kmer frequencies in final table
+PVAL=0.01     #P-value cut off, vales equal or smaller than this go to signifcant sex differences table
+LOWCOV=0      #Set to 1 for low sequencing coverage data (1-6x) per individual (genome skimming approaches), may increase runtime and disk usage!
+
+#creating list of readfiles per sample id (for low coverage data we just use each file twice to meet the minimum kmer criteria of 2!)
+if [ $LOWCOV -eq 0 ]
+then
+        echo "USING DEFAULT MODE"
+        find males/| grep fq.gz$ | sort -V | awk '{split($1,a,/[/_]/);print $1 > a[2]".list"}'
+        find females/| grep fq.gz$ | sort -V | awk '{split($1,a,/[/_]/);print $1 > a[2]".list"}'
+else
+        echo "USING LOW COVERAGE MODE"
+        find males/| grep fq.gz$ | sort -V | awk '{split($1,a,/[/_]/);print $1"\n"$1 > a[2]".list"}'
+        find females/| grep fq.gz$ | sort -V | awk '{split($1,a,/[/_]/);print $1"\n"$1 > a[2]".list"}'
+fi
+
+#CREATING KMC RUNs per sample
+ls *.list | awk -v cpu=$KMCTHREADS '{print "kmc -t"cpu" -k27 -r @"$1" "$1".res ./kmc_tmp_dir/ > "$1".res.log 2>&1"}' > KMC_BATCH.sh
+
+#executing KMC runs
+mkdir ./kmc_tmp_dir
+#not more than 4 parallel jobs! IO is limiting! Instead more kmc threads (i.e. -t20 )
+cat KMC_BATCH.sh | nohup parallel -j $KMCJOBS
+
+#create operations file for union of all kmers
+find males/ females/ | grep fq.gz | sort -V | cut -f 1 -d '_' | uniq | awk 'BEGIN{print "INPUT:";}{n=split($1,a,"/");if(a[1]=="males"){i++;print "m"i" = " a[2]".list.res";text=text"m"i" + "} else if(a[1]=="females"){j++;print "f"j" = " a[2]".list.res";text=text"f"j" + "}} END{print "OUTPUT:\nallunion = "substr(text,1,length(text)-2)"\nOUTPUT_PARAMS:\n-cs1000000"}' > union-op-def.file
+
+#execute union (may use lots of memory here)
+kmc_tools complex union-op-def.file
+
+#remove counts from union
+kmc_tools transform allunion compact allunion-nocounts
+
+#replace counts of all kmers with those of single samples (no match kmers will have count "1" !!!)
+#and dump all counts to gzipped files (8 threads in parallel work!)
+#the combination of these steps saves disk space (increasing parallel threads will use more disk for temporary kmc databases!)
+ls *list | awk '{n=$1;gsub(".list","",n);print "kmc_tools simple allunion-nocounts -ci0 "$1".res -ci0 union all_"n" -ocright; kmc_dump all_"n" /dev/stdout | cut -f 2 | pigz -c > all_"n".counts.gz; rm all_"n"*kmc_*"}' | nohup parallel -j $COUNTJOBS
+
+#remove old KMC-DBs
+rm *res* -f
+
+#dump all kmers for once
+kmc_dump allunion /dev/stdout | cut -f 1 | pigz -c > all.kmers.gz
+
+#free disk
+rm -f *.kmc_suf *.kmc_pre
+
+#dump to table
+#make fifos
+awk '{if($1!="INPUT:" && $1!="OUTPUT:"){print};if($1=="OUTPUT:"){exit;}}' union-op-def.file | awk 'BEGIN{printf "mkfifo "} {printf $1" "}' | bash
+#gunzip to fifo's
+awk '{if($1!="INPUT:" && $1!="OUTPUT:"){print};if($1=="OUTPUT:"){exit;}}' union-op-def.file | awk '{gsub(".list.res",".counts.gz",$3);print "gunzip -c all_"$3" > "$1" &"}' | bash
+mkfifo start
+gunzip -c all.kmers.gz > start &
+
+#final table merge, Kmer-positive female/male counting and compress
+export FCOUNT="$(awk '{if($1!="INPUT:" && $1!="OUTPUT:"){print};if($1=="OUTPUT:"){exit;}}' union-op-def.file | grep ^f | wc -l)"
+awk '{if($1!="INPUT:" && $1!="OUTPUT:"){print};if($1=="OUTPUT:"){exit;}}' union-op-def.file | awk 'BEGIN{text="start";} {text=text" "$1;} END{print "paste "text; }' | bash | parallel --tmpdir ./ --keep-order -j $MERGEJOBS -l 1000000 --pipe 'mawk -v cmin1=2 -v cmin2=1 -v fcount=$FCOUNT -f count.samples.awk - ' | mawk -v pval=$PVAL '{if(pval!="" && $1<=pval) {print | "pigz -c > Pval"pval".tsv.gz"}; print | "pigz -c > FINAL-table.tsv.gz"}'
+
+#clean-up
+#remove FIFOs
+rm -f start f? f?? f??? m? m?? m???
+#remove lists and related
+rm -f *list* nohup.out
+#remove tmp-dir
+rm kmc_tmp_dir/ -rf
+#remove temporary count-files
+rm -f *.counts.gz all.kmers.gz

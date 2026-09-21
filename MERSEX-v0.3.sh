188e53c803a05bdb13a1c96a6983f2f63aaebb3a
HMPNK <51913753+HMPNK@users.noreply.github.com>
2026-09-21 12:20:07 +0200

    Update MERSEX-v0.3.sh

--- MERSEX-v0.3.sh
+++ MERSEX-v0.3.sh
@@ -56,4 +56,4 @@ echo "Number of   male samples: "$MCOUNT
 ./kmc_merge -b 131072 -S 131072 -L 4097 -t $MERGE -z -o KMCMERGED.tsv.gz $KMCDBS
 
 #Extract significant sex differentiating kmers:
-pigz -dc KMCMERGED.tsv.gz | parallel --tmpdir ./ --keep-order -j $PJOBS -l 1000000 --pipe 'mawk -v cmin1=$KPOS -v cmin2=$KNEG -v fcount=$FCOUNT -f count.samples.awk - ' | mawk -v pval=$PVAL '{if(pval!="" && $1<=pval) {print | "pigz -c > Pval"pval".tsv.gz"};}'
+pigz -dc KMCMERGED.tsv.gz | ./fisher_mt -f $FCOUNT -H -p $PVAL -t $PJOBS --cmin1 $KPOS --cmin2 $KNEG | pigz -c > KMCMERGED_pval$PVAL.tsv.gz

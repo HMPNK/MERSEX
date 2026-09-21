ebc441b06735f13ff391f976b5022cd0f3e49337
HMPNK <51913753+HMPNK@users.noreply.github.com>
2026-09-21 12:13:35 +0200

    Update COMPILE.sh

--- KMC-MERGE-TOOL-HMPNK/COMPILE.sh
+++ KMC-MERGE-TOOL-HMPNK/COMPILE.sh
@@ -1,2 +1,4 @@
 g++ -std=c++17 -O3 -march=native -flto -pthread -Ikmc_api kmc_merge_final.cpp ./kmc_api/*.cpp -o kmc_merge
 mv kmc_merge ../
+g++ -O2 -std=c++17 -pthread -o fisher_mt fisher_mt.cpp
+mv fisher_mt ../
